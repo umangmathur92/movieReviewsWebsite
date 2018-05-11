5e3fc77f54eb3df2e327d15951bf82a5b3535db4
umangmathur92 <umangmathur10051992@gmail.com>
2018-05-10 20:20:27 -0700

    added config file

---
+++ config/config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "mysql"
+  },
+  "test": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "mysql"
+  }
+};
