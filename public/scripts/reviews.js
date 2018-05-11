3a2ee3cfbb8d1a56b3081650bfadeaf0854d7342
umangmathur92 <umangmathur10051992@gmail.com>
2018-05-11 12:06:22 -0700

    fixed date formatting

--- public/scripts/reviews.js
+++ public/scripts/reviews.js
@@ -21,9 +21,15 @@ function generateListings(reviewList) {
             '<h3 class="li_title">' + review.title + '</h3>' +
             '<h6 class="li_title">' + 'Email: ' + review.email + '</h6>' +
             '<h6 class="li_title">' + 'Rating: ' + review.score + '</h6>' +
-            '<h6 class="li_title">' + 'Review Date: '  + review.timestamp + '</h6>' +
+            '<h6 class="li_title">' + 'Review Date: '  + getFormattedDateString(review.timestamp) + '</h6>' +
 			'<h6 class="li_title">' + 'Detailed Review: '   + review.content + '</h6>' +     
 			'</li>'
 		);
     });
+}
+
+function getFormattedDateString(inputDateStr) {
+	//Input date string format: YYYY-MM-DDTHH:MM:SSZ
+	var date = Date.parse(inputDateStr); 
+	return moment(date).format('MMMM DD, YYYY');
 }
