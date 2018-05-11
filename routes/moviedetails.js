41ead525f6de27ba8a5013a751b80f19c3731d0f
umangmathur92 <umangmathur10051992@gmail.com>
2018-05-11 11:24:17 -0700

    displayed movie details

--- routes/moviedetails.js
+++ routes/moviedetails.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var dbConnection = require('../db/database');
+const dateFormat = require('dateformat');
 
 router.get('/:mid', function(req, res, next) {
     var midd = req.params.mid;
-    var query = 'SELECT * from movie_directs WHERE mid='+midd;
+    var query = 'SELECT m.mid, m.name, m.rel_date, m.url, m.description, d.name AS dir_name , AVG(score) AS avg_score ' +
+    'FROM movie_directs m, directors d, review_refersto_writes r ' +
+    'WHERE m.did=d.did AND r.mid=m.mid AND m.mid=' + midd + ' ' +
+    'GROUP BY m.mid';
     console.log(query);
     dbConnection.query(query, function(err, rows, fields) {
         if (!err) {
-            res.send(rows);
+            //res.send(rows[0]);
+            var data = rows[0];
+            data.rel_date = getFormattedDateString(data.rel_date);
+            res.render('moviedetails', data);
         } else {
             res.send('Error while performing Query.');
         }
     });
 });
+
+function getFormattedDateString(inputDate) {
+	return dateFormat(inputDate, 'mmmm d, yyyy');
+}
 
 module.exports = router;
